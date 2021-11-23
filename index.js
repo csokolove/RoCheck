129fa96d6bbabfa0c9a3140052fc8f028cf9136e
csokolove <94080789+csokolove@users.noreply.github.com>
2021-11-23 10:27:59 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const chalk = require('chalk');
+const clear = require('clear');
+const figlet = require('figlet');
+
+clear();
+
+console.log(
+  chalk.red(
+    figlet.textSync('RoCheck', { horizontalLayout: 'full' })
+  )
+);
+
+const check = require('./lib/check');
+
+check();
